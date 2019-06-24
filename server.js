04ded58d2c455bfe67bb7500fb9383df231603f4
Christensen-Dustin <39143972+Christensen-Dustin@users.noreply.github.com>
2019-06-24 16:35:46 -0700

    Prepare 10 POOL and PG update

---
+++ server.js
@@ -0,0 +1,18 @@
+const connectionString = process.env.DATABASE_URL || "postgres://mboyxxguszqiuc:5923050be96786641b297de723b4e9b90e7380e336a34c8b637dd853a04d912a@ec2-23-21-109-177.compute-1.amazonaws.com:5432/d83naof1orve8s?ssl=true";
+
+const pool = new Pool({connectionString: connectionString});
+
+var sql = "SElECT * FROM accountEvent";
+
+pool.query(sql, function(err, result) {
+    // If an error occurred....
+    if (err) {
+        console.log("Error in query: ")
+        console.log(err);
+    }
+    
+    // Log this to the console for debugging purposes.
+    console.log("Back from DB with results: ");
+    console.log(result.rows);
+    
+});

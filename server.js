57686971c545b5de6eef4121e6374069dbb89132
Christensen-Dustin <39143972+Christensen-Dustin@users.noreply.github.com>
2019-06-24 16:42:02 -0700

    Prepare 10 POOL and PG update

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
-const { Pool } = require('pg');
-const pool = new Pool();
-
 const connectionString = process.env.DATABASE_URL || "postgres://mboyxxguszqiuc:5923050be96786641b297de723b4e9b90e7380e336a34c8b637dd853a04d912a@ec2-23-21-109-177.compute-1.amazonaws.com:5432/d83naof1orve8s?ssl=true";
 
+const { Pool } = require('pg');
 const pool = new Pool({connectionString: connectionString});
 
 var sql = "SElECT * FROM accountEvent";
